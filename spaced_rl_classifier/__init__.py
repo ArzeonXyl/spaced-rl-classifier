7b89e8c92cef5ca5f68322d2eb7eb8ef9f335cf8
ArzeonX <160443046+ArzeonXyl@users.noreply.github.com>
2025-12-02 18:41:22 +0700

    Create __init__.py

---
+++ spaced_rl_classifier/__init__.py
@@ -0,0 +1,8 @@
+# spaced_rl_classifier/__init__.py
+
+# import semua class utama supaya bisa langsung dipanggil saat package diimport
+from .spaced_rl_classifier import (
+    SpacedRLClassifier,
+    SimpleMLP,
+    RLSchedulerPolicy
+)
